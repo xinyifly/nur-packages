c8b1f5c91bca5fc967b82aecae8f7bc31b0ad4e3
Zeyu Chen <xinyifly@live.cn>
2023-04-04 02:02:37 +0800

    feat(modules): xray-redir use latest xray

--- modules/xray-redir.nix
+++ modules/xray-redir.nix
@@ -84,6 +84,19 @@ in {
       };
     };
     systemd.services.xray = let
+      xray = stdenv.mkDerivation {
+        name = "xray";
+        src = fetchzip {
+          url = "https://github.com/XTLS/Xray-core/releases/download/"
+            + "v1.7.5/Xray-linux-64.zip";
+          hash = "sha256-ZCkHZcV9hwATXRVAHZ/hj8CotE/lDU9WQal0K5bLBvM=";
+          stripRoot = false;
+        };
+        installPhase = ''
+          mkdir -p $out/bin
+          cp xray $out/bin/
+        '';
+      };
       basicConfig = {
         inbounds = [{
           port = 1024;
