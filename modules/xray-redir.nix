e25ebfbc0acd11557b69e02f3e7375bd34fcf2ad
Zeyu Chen <xinyifly@live.cn>
2023-04-16 05:54:58 +0800

    feat(modules): xray-redir route openai

--- modules/xray-redir.nix
+++ modules/xray-redir.nix
@@ -1,256 +1,201 @@
 { config, pkgs, lib, ... }:
 
-with pkgs;
-with lib;
+with builtins;
 let
   cfg = config.services.xray-redir;
-  recursiveMerge = sets:
-    zipAttrsWith (name: values:
-      if all isList values then
-        concatLists values
-      else if all isAttrs values then
-        recursiveMerge values
-      else
-        last values) sets;
-in {
-  options = with types; {
-    services.xray-redir = {
-      enable = mkOption {
-        type = bool;
-        default = false;
+  outbounds = let
+    parse = host:
+      with lib;
+      let arr = splitString ":" host;
+      in {
+        address = head arr;
+        port = toInt (head (tail arr));
       };
-      vision = mkOption {
-        type = attrs;
-        default = { };
+  in map (server:
+    if server.type == "vision" then {
+      tag = server.tag;
+      protocol = "vless";
+      settings = {
+        vnext = let host = parse server.host;
+        in [{
+          address = host.address;
+          port = host.port;
+          users = [{
+            id = server.user;
+            flow = "xtls-rprx-vision";
+            encryption = "none";
+          }];
+        }];
       };
-      dns = mkOption { type = listOf str; };
-      ignores = mkOption {
-        type = listOf str;
-        default = [ ];
+      streamSettings = { security = "tls"; };
+    } else if server.type == "ssrdog" then {
+      tag = server.tag;
+      protocol = "vmess";
+      settings = {
+        vnext = let host = parse server.host;
+        in [{
+          address = host.address;
+          port = host.port;
+          users = [{ id = server.user; }];
+        }];
       };
-      xrayConfig = mkOption {
-        type = attrs;
-        default = { };
+      streamSettings = {
+        network = "ws";
+        wsSettings = {
+          connectionReuse = true;
+          path = "";
+          headers = { Host = ""; };
+        };
       };
-      overtureConfig = mkOption {
-        type = attrs;
-        default = { };
+      mux = { enabled = true; };
+    } else
+      { }) cfg.servers;
+  defaults = {
+    inbounds = [{
+      port = 12345;
+      protocol = "dokodemo-door";
+      settings = {
+        network = "tcp,udp";
+        followRedirect = true;
       };
+      streamSettings = { sockopt = { tproxy = "tproxy"; }; };
+    }];
+    outbounds = outbounds ++ [
+      {
+        tag = "direct";
+        protocol = "freedom";
+      }
+      {
+        tag = "dns";
+        protocol = "dns";
+      }
+    ];
+    dns = {
+      servers = [
+        {
+          address = "localhost";
+          expectIPs = [ "geoip:private" "geoip:cn" ];
+        }
+        "8.8.8.8"
+        "1.1.1.1"
+        {
+          address = "8.8.4.4";
+          domains = [ "domain:openai.com" ];
+        }
+      ];
+    };
+    routing = {
+      rules = [
+        {
+          type = "field";
+          ip = [ "geoip:private" "geoip:cn" ];
+          network = "udp";
+          port = "53";
+          outboundTag = "dns";
+        }
+        {
+          type = "field";
+          ip = [ "geoip:private" "geoip:cn" ];
+          outboundTag = "direct";
+        }
+        {
+          type = "field";
+          ip = [ "8.8.4.4" "104.18.2.161" "104.18.3.161" ];
+          outboundTag = "us";
+        }
+      ];
     };
   };
-  config = let
-    china-ip-list = stdenv.mkDerivation {
-      name = "china-ip-list";
-      src = fetchFromGitHub {
-        owner = "17mon";
-        repo = "china_ip_list";
-        rev = "78c3d7855678f058e5ca861e0a28aa9800dba308";
-        hash = "sha256-wAymDtFKLzxudF+ypo5qtpO4GdGWw0cRo/7xYq2nHoU=";
-      };
-      installPhase = ''
-        mkdir -p $out
-        cp china_ip_list.txt $out/ignore.list
-        cat <<EOF >> $out/ignore.list
-        10.0.0.0/8
-        100.64.0.0/10
-        127.0.0.0/8
-        169.254.0.0/16
-        172.16.0.0/12
-        192.0.0.0/24
-        192.168.0.0/16
-        224.0.0.0/4
-        240.0.0.0/4
-        255.255.255.255/32
-        EOF
-        cat <<EOF >> $out/ignore.list
-        ${strings.concatStringsSep "\n" cfg.ignores}
-        EOF
-      '';
-    };
-  in mkIf cfg.enable {
-    systemd.services.ipset = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      path = [ ipset china-ip-list ];
-      script = ''
-        ipset create ignore hash:net
-        for ip in `cat ${china-ip-list}/ignore.list`; do
-            ipset add ignore $ip
-        done
-      '';
-      preStop = "ipset destroy ignore";
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-      };
-    };
-    systemd.services.xray = let
-      xray = stdenv.mkDerivation {
-        name = "xray";
-        src = fetchzip {
-          url = "https://github.com/XTLS/Xray-core/releases/download/"
-            + "v1.7.5/Xray-linux-64.zip";
-          hash = "sha256-ZCkHZcV9hwATXRVAHZ/hj8CotE/lDU9WQal0K5bLBvM=";
-          stripRoot = false;
+in {
+  options = {
+    services.xray-redir = with lib;
+      with types; {
+        enable = mkOption {
+          type = bool;
+          default = false;
         };
-        installPhase = ''
-          mkdir -p $out/bin
-          cp xray $out/bin/
-        '';
+        servers = mkOption { type = listOf attrs; };
+        settings = mkOption {
+          type = attrs;
+          default = { };
+        };
       };
-      basicConfig = {
-        inbounds = [{
-          port = 1024;
-          protocol = "dokodemo-door";
-          settings = {
-            network = "tcp,udp";
-            followRedirect = true;
-          };
-          streamSettings = { sockopt = { tproxy = "tproxy"; }; };
-        }];
-        outbounds = if cfg.vision == { } then
-          [ ]
-        else [{
-          protocol = "vless";
-          settings = {
-            vnext = [{
-              address = cfg.vision.address;
-              port = cfg.vision.port;
-              users = [{
-                id = cfg.vision.user;
-                flow = "xtls-rprx-vision";
-                encryption = "none";
-              }];
-            }];
-          };
-          streamSettings = {
-            security = "tls";
-            sockopt = { mark = 2; };
-          };
-        }];
-      };
-      xrayConfig = writeText "xray-config.json"
-        (builtins.toJSON (recursiveMerge [ basicConfig cfg.xrayConfig ]));
+  };
+  config = lib.mkIf cfg.enable {
+    users.groups.xray.gid = 23333;
+    services.xray = let
     in {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" "ipset.service" ];
-      requires = [ "ipset.service" ];
-      path = [ xray iproute2 iptables ];
-      script = ''
-        ulimit -n 65536
+      enable = true;
+      package = with pkgs;
+        (stdenv.mkDerivation rec {
+          name = "xray-${version}";
+          version = "1.7.5";
+          src = fetchzip {
+            url = "https://github.com/XTLS/Xray-core/releases/download/"
+              + "v${version}/Xray-linux-64.zip";
+            hash = "sha256-ZCkHZcV9hwATXRVAHZ/hj8CotE/lDU9WQal0K5bLBvM=";
+            stripRoot = false;
+          };
+          installPhase = ''
+            mkdir -p $out/bin
+            cp xray geoip.dat geosite.dat $out/bin/
+          '';
+        });
+      settings = let
+        recursiveMerge = sets:
+          with lib;
+          zipAttrsWith (name: values:
+            if all isList values then
+              concatLists values
+            else if all isAttrs values then
+              recursiveMerge values
+            else
+              head values) sets;
+      in recursiveMerge [ cfg.settings defaults ];
+    };
+    systemd.services.xray = {
+      path = with pkgs; [ iproute2 iptables gawk ];
+      serviceConfig = {
+        User = "root";
+        Group = "xray";
+        DynamicUser = lib.mkForce false;
+        LimitNOFILE = 65536;
+      };
+      preStart = ''
         ip rule add fwmark 1 table 100
         ip route add local 0.0.0.0/0 dev lo table 100
 
         iptables -w -t mangle -N XRAY
-        iptables -w -t mangle -A XRAY -m set --match-set ignore dst -j RETURN
-        iptables -w -t mangle -A XRAY -p tcp -j TPROXY --on-port 1024 --tproxy-mark 1
-        iptables -w -t mangle -A XRAY -p udp -j TPROXY --on-port 1024 --tproxy-mark 1
+        for ip in $(ip address | grep -w inet | awk '{print $2}'); do
+            iptables -w -t mangle -A XRAY -p tcp -d $ip -j RETURN
+            iptables -w -t mangle -A XRAY -p udp ! --dport 53 -d $ip -j RETURN
+        done
+        iptables -w -t mangle -A XRAY -p tcp -j TPROXY --on-port 12345 --tproxy-mark 1
+        iptables -w -t mangle -A XRAY -p udp -j TPROXY --on-port 12345 --tproxy-mark 1
         iptables -w -t mangle -A PREROUTING -j XRAY
 
-        iptables -w -t mangle -N XRAY_SELF
-        iptables -w -t mangle -A XRAY_SELF -m mark --mark 2 -j RETURN
-        iptables -w -t mangle -A XRAY_SELF -m set --match-set ignore dst -j RETURN
-        iptables -w -t mangle -A XRAY_SELF -p tcp -j MARK --set-mark 1
-        iptables -w -t mangle -A XRAY_SELF -p udp -j MARK --set-mark 1
-        iptables -w -t mangle -A OUTPUT -j XRAY_SELF
-
-        exec xray -c ${xrayConfig}
+        iptables -w -t mangle -N XRAY_MASK
+        iptables -w -t mangle -A XRAY_MASK -m owner --gid-owner 23333 -j RETURN
+        for ip in $(ip address | grep -w inet | awk '{print $2}'); do
+            iptables -w -t mangle -A XRAY_MASK -p tcp -d $ip -j RETURN
+            iptables -w -t mangle -A XRAY_MASK -p udp ! --dport 53 -d $ip -j RETURN
+        done
+        iptables -w -t mangle -A XRAY_MASK -p tcp -j MARK --set-mark 1
+        iptables -w -t mangle -A XRAY_MASK -p udp -j MARK --set-mark 1
+        iptables -w -t mangle -A OUTPUT -j XRAY_MASK
       '';
       preStop = ''
         ip rule del fwmark 1 table 100
         ip route del local 0.0.0.0/0 dev lo table 100
 
-        iptables -t mangle -D PREROUTING -j XRAY
-        iptables -t mangle -F XRAY
-        iptables -t mangle -X XRAY
+        iptables -w -t mangle -D PREROUTING -j XRAY
+        iptables -w -t mangle -F XRAY
+        iptables -w -t mangle -X XRAY
 
-        iptables -t mangle -D OUTPUT -j XRAY_SELF
-        iptables -t mangle -F XRAY_SELF
-        iptables -t mangle -X XRAY_SELF
+        iptables -w -t mangle -D OUTPUT -j XRAY_MASK
+        iptables -w -t mangle -F XRAY_MASK
+        iptables -w -t mangle -X XRAY_MASK
       '';
     };
-    systemd.services.overture = let
-      overture = stdenv.mkDerivation {
-        name = "overture";
-        src = fetchzip {
-          url = "https://github.com/shawn1m/overture/releases/download/"
-            + "v1.8/overture-linux-amd64.zip";
-          hash = "sha256-yoVvopu4lcw4jbOetQ+Ktir9tAtJ0nd9dEekSeQF1C8=";
-          stripRoot = false;
-        };
-        installPhase = let
-          basicConfig = {
-            bindAddress = "127.0.0.1:53";
-            debugHTTPAddress = "127.0.0.1:5555";
-            dohEnabled = false;
-            primaryDNS = [{
-              name = "PrimaryDNS";
-              address = head cfg.dns;
-              protocol = "udp";
-              socks5Address = "";
-              timeout = 30;
-              ednsClientSubnet = {
-                policy = "disable";
-                externalIP = "";
-                noCookie = true;
-              };
-            }];
-            alternativeDNS = [{
-              name = "AlternativeDNS";
-              address = last cfg.dns;
-              protocol = "tcp";
-              socks5Address = "";
-              timeout = 30;
-              ednsClientSubnet = {
-                policy = "disable";
-                externalIP = "";
-                noCookie = true;
-              };
-            }];
-            onlyPrimaryDNS = false;
-            ipv6UseAlternativeDNS = false;
-            alternativeDNSConcurrent = true;
-            whenPrimaryDNSAnswerNoneUse = "alternativeDNS";
-            ipNetworkFile = {
-              primary = "./ip_network_primary_sample";
-              alternative = "./ip_network_alternative_sample";
-            };
-            domainFile = {
-              primary = "./domain_primary_sample";
-              alternative = "./domain_alternative_sample";
-              matcher = "full-map";
-            };
-            hostsFile = {
-              hostsFile = "./hosts_sample";
-              finder = "full-map";
-            };
-            minimumTTL = 0;
-            domainTTLFile = "./domain_ttl_sample";
-            cacheSize = 4096;
-            cacheRedisUrl = "";
-            cacheRedisConnectionPoolSize = 0;
-            rejectQType = [ 255 ];
-          };
-          overtureConfig = writeText "overture-config.yml"
-            (generators.toYAML { }
-              (recursiveMerge [ basicConfig cfg.overtureConfig ]));
-        in ''
-          mkdir -p $out/bin
-          cp overture-linux-amd64 $out/bin/overture
-          mkdir -p $out/etc
-          cp config.yml *_sample $out/etc/
-          ln -sf ${overtureConfig} $out/etc/config.yml
-          ln -sf ${china-ip-list}/ignore.list $out/etc/ip_network_primary_sample
-        '';
-      };
-    in {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      path = [ overture ];
-      script = "exec overture";
-      serviceConfig = { WorkingDirectory = "${overture}/etc"; };
-    };
-    networking.nameservers = [ "127.0.0.1" ];
-    boot.kernel.sysctl."net.ipv6.conf.all.disable_ipv6" = 1;
   };
 }
